281fd1535242ff942311a719f8ce42c8df81886b
Citrofiska <bjwx29@163.com>
2024-06-12 21:04:49 +0200

    change batch size default

--- src/train.py
+++ src/train.py
@@ -24,7 +24,7 @@ parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs 
 parser.add_argument("--model_name", type=str, help="name of the model")
 parser.add_argument("--dataset", type=str, help="path to dataset for training")
 parser.add_argument("--n_spkrs", type=int, default=2, help="number of speakers for conversion, must match that of preprocessing")
-parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=2, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
